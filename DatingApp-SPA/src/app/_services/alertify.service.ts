4b4f990f45c2ac56e44117ba8d1fa02a67be3ea3
Marek Galarowicz <marek.galarowicz@enova.pl>
2019-01-04 22:22:04 +0100

    ThirdPartyComponents

---
+++ DatingApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+declare let alertify: any;
+
+@Injectable({
+  providedIn: "root"
+})
+export class AlertifyService {
+  constructor() {}
+
+  confirm(message: string, okCallback: () => any) {
+    alertify.confirm(message, function(e) {
+      if (e) {                                  /** e represents user clicking the ok button. ok button click event*/
+        okCallback();
+      } else {
+      }
+    });
+  }
+
+  success(message: string) {
+    alertify.success(message);
+  }
+
+  error(message: string) {
+    alertify.error(message);
+  }
+
+  warning(message: string) {
+    alertify.warning(message);
+  }
+
+  message(message: string) {
+    alertify.message(message);
+  }
+}
