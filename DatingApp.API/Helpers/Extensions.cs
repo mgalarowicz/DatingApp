8d16b2d1dbbb48c37a0cf27668ad8f5493cd30a8
Marek Galarowicz <marek.galarowicz@enova.pl>
2019-01-07 19:47:37 +0100

    SeedUsers

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -9,6 +10,15 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Application-Error", message);  //naglowek headera i jego tresc
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");   //komunikacja z Angularem, zezwalaja nam na wystwietlenie headera
             response.Headers.Add("Access-Control-Allow-Origin", "*");       //komunikacja z Angularem, zezwalaja nam na wystwietlenie headera 
+        }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age) > DateTime.Today)
+            age--;
+            
+            return age;
         }
     }
 }
